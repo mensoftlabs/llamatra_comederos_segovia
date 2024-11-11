e93a1e7dbeb566290f9fc5a135cdd367239abb8e
Carmen Piq <149472329+Carmenpiq@users.noreply.github.com>
2024-11-11 09:40:58 +0100

    a

--- code_mux_comederos/main/main.c
+++ code_mux_comederos/main/main.c
@@ -6,22 +6,27 @@
 
 // Definir los pines UART
 #define UART_NUM UART_NUM_1
-#define UART_TX_PIN GPIO_NUM_18  //CLK (TX del micro)
+#define UART_TX_PIN GPIO_NUM_13  //SENSE1 (TX del micro) 
 #define UART_RX_PIN GPIO_NUM_19  //MISO (RX del micro)
 #define UART_INH_PIN GPIO_NUM_23 //MOSI 
 
 // Definir los pines de control del MUX
-#define PIN_A GPIO_NUM_13        //SENSE1
-//#define PIN_B GPIO_NUM_15        //SENSE2
+#define PIN_A GPIO_NUM_18        //CLK
 #define PIN_B GPIO_NUM_5         //SD_CS
-//#define PIN_C GPIO_NUM_14        //SENSE3
 #define PIN_C GPIO_NUM_22        //SCL
 
 // Tamaño del buffer
 #define BUF_SIZE (1024)
 
+// Definir el número de sensores para multiplexar
+#define NUM_SENSE 5
+
+//Variables globales
+uint8_t canal = 0; 
+uint8_t cont1, cont2, cont3, cont4, cont5;
+
 // Función para procesar un paquete de datos del sensor
-bool process_data_packet(uint8_t *data, int length) {
+uint8_t process_data_packet(uint8_t *data, int length) {
     for (int i = 0; i < length; i += 4) {
         if (data[i] == 0xFF && i + 3 < length) {  // Verificar header y longitud suficiente para el paquete
             uint8_t Data_H = data[i + 1];
@@ -31,24 +36,30 @@ bool process_data_packet(uint8_t *data, int length) {
             uint8_t calculated_checksum = (0xFF + Data_H + Data_L) & 0xFF;
 
             if (calculated_checksum == checksum) {
-                uint8_t distance = (Data_H << 8) | Data_L;
+                uint16_t distance = (Data_H << 8) | Data_L;
                 uint8_t umbral = 100;
                 bool pig;
-                printf("Distancia: %d mm\n", distance);
+                printf(" %d mm\n", distance);
 
                 if (distance > umbral) {
                     pig = 0;
                 } else {
                     pig = 1;
+                    if (distance == 0) {
+                      pig = 0;
+                    }
                   }
                 printf("Hay cerdo: %d \n", pig);
+                printf("\n");
 
                 return pig; // Devolver la variable que decide si el cerdo está bebiendo
             } else {
-                printf("Checksum incorrecto en el paquete %d\n", i / 4);
+                printf("Checksum incorrecto en el paquete \n");
+                printf("\n");
               }
         } else {
-            printf("Paquete de datos incorrecto en el índice %d\n", i);
+            printf("Paquete de datos incorrecto \n");
+            printf("\n");
           }
     }
     return 0; // Devolver 0 si no se pudo procesar ningún paquete correctamente
@@ -56,16 +67,38 @@ bool process_data_packet(uint8_t *data, int length) {
 
 // Función para configurar los pines de control A, B y C del MUX
 void configure_multiplexer() {
-    gpio_set_direction(PIN_A, GPIO_MODE_OUTPUT);
-    gpio_set_direction(PIN_B, GPIO_MODE_OUTPUT);
-    gpio_set_direction(PIN_C, GPIO_MODE_OUTPUT);
-    gpio_set_direction(UART_INH_PIN, GPIO_MODE_OUTPUT);
 
-    // Seleccionar el canal Y0 (A=0, B=0, C=0)
-    gpio_set_level(PIN_A, 0);
-    gpio_set_level(PIN_B, 0);
-    gpio_set_level(PIN_C, 1);
-    gpio_set_level(UART_INH_PIN, 0);
+    switch (canal) {
+        case 0: //Sensor 1 en el pin Y0
+          gpio_set_level(PIN_A, 0);
+          gpio_set_level(PIN_B, 0);
+          gpio_set_level(PIN_C, 0);
+          break;
+        case 1: //Sensor 2 en el pin Y1
+          gpio_set_level(PIN_A, 1);
+          gpio_set_level(PIN_B, 0);
+          gpio_set_level(PIN_C, 0);
+          break;
+        case 2: //Sensor 3 en el pin Y2
+          gpio_set_level(PIN_A, 0);
+          gpio_set_level(PIN_B, 1);
+          gpio_set_level(PIN_C, 0);
+          break;
+        case 3: //Sensor 4 en el pin Y3
+          gpio_set_level(PIN_A, 1);
+          gpio_set_level(PIN_B, 1);
+          gpio_set_level(PIN_C, 0);
+          break;
+        case 4: //Sensor 5 en el pin Y4
+          gpio_set_level(PIN_A, 1);
+          gpio_set_level(PIN_B, 1);
+          gpio_set_level(PIN_C, 0);
+          break;
+    }
+
+    printf("Canal actual %d: ", canal);
+
+    gpio_set_level(UART_INH_PIN, 0); //Habilitar el mux
 }
 
 // Función para configurar el UART
@@ -89,13 +122,16 @@ bool read_ultrasonic_sensor(void) {
     // Buffer para almacenar los datos del UART
     uint8_t data[BUF_SIZE];
 
-    // Leer datos del sensor
-    int length = uart_read_bytes(UART_NUM, data, BUF_SIZE, 100 / portTICK_PERIOD_MS);
+    // Leer datos del sensor y limpiar buffer
+    int length = uart_read_bytes(UART_NUM, data, 92, 100 / portTICK_PERIOD_MS);
+    uart_flush_input(UART_NUM);
+
     if (length > 0) {
         // Procesar los datos según el protocolo del sensor y devolver la distancia
         return process_data_packet(data, length);
     } else {
         printf("No data received\n");
+        printf("\n");
       }
 
     // Si no se reciben datos, devolver 0
@@ -104,21 +140,24 @@ bool read_ultrasonic_sensor(void) {
 
 // Función principal
 void app_main(void) {
+
+    gpio_set_direction(PIN_A, GPIO_MODE_OUTPUT);
+    gpio_set_direction(PIN_B, GPIO_MODE_OUTPUT);
+    gpio_set_direction(PIN_C, GPIO_MODE_OUTPUT);
+
+    gpio_set_level(UART_TX_PIN, 0); //Fijar tiempo de respuesta a 100ns
+
     // Configurar UART y MUX solo una vez
     configure_uart();
-    configure_multiplexer();
 
     while (1) {
-        // Llamar a la función de lectura del sensor y almacenar el resultado
-        bool pig_presence = read_ultrasonic_sensor();
+        configure_multiplexer();
+        read_ultrasonic_sensor();
+
+        //Cambia al siguiente canal
+        canal = (canal + 1) % NUM_SENSE;
         
-    /*    //PRUEBA PARA VER SI TX DEL ESP32 FUNCIONA
-        uint8_t byte_to_send = 0xAA;  // Byte a enviar
-        // Enviar el byte por UART
-        uart_write_bytes(UART_NUM, (const char *)&byte_to_send, 1); //Enviar caracter "..." continuamente
-    */
-        // Esperar un segundo antes de la siguiente lectura
-        vTaskDelay(pdMS_TO_TICKS(100));
+        // Esperar antes de la siguiente lectura
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
-

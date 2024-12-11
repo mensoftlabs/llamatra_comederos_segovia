3430881abca336d2602779231e4bd71b817c5501
Carmen Piq <149472329+Carmenpiq@users.noreply.github.com>
2024-12-11 14:08:09 +0100

    opt

--- code_mux_comederos/main/main.c
+++ code_mux_comederos/main/main.c
@@ -30,132 +30,132 @@ uint8_t umbral = 100;
 
 // Función para configurar el UART
 void configure_uart(void) {
-    const uart_config_t uart_config = {
-        .baud_rate = 9600,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        //.source_clk = UART_SCLK_APB, // Especificar el reloj APB como fuente
-        .source_clk = UART_SCLK_DEFAULT
-    };
-    uart_param_config(UART_NUM, &uart_config);
-    uart_set_pin(UART_NUM, UART_TX_PIN, UART_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    uart_driver_install(UART_NUM, BUF_SIZE, BUF_SIZE, 0, NULL, 0);
+  const uart_config_t uart_config = {
+    .baud_rate = 9600,
+    .data_bits = UART_DATA_8_BITS,
+    .parity = UART_PARITY_DISABLE,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    //.source_clk = UART_SCLK_APB, // Especificar el reloj APB como fuente
+    .source_clk = UART_SCLK_DEFAULT
+  };
+  uart_param_config(UART_NUM, &uart_config);
+  uart_set_pin(UART_NUM, UART_TX_PIN, UART_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+  uart_driver_install(UART_NUM, BUF_SIZE, BUF_SIZE, 0, NULL, 0);
 }
 
 // Función para procesar un paquete de datos del sensor
 uint16_t process_data_packet(uint8_t *data, int length) {
-    for (int i = 0; i < length; i += 4) {
-        if (data[i] == 0xFF && i + 3 < length) {  // Verificar header y longitud suficiente para el paquete
-            uint8_t Data_H = data[i + 1];
-            uint8_t Data_L = data[i + 2];
-            uint8_t checksum = data[i + 3];
+  for (int i = 0; i < length; i += 4) {
+    if (data[i] == 0xFF && i + 3 < length) {  // Verificar header y longitud suficiente para el paquete
+      uint8_t Data_H = data[i + 1];
+      uint8_t Data_L = data[i + 2];
+      uint8_t checksum = data[i + 3];
 
-            uint8_t calculated_checksum = (0xFF + Data_H + Data_L) & 0xFF;
+      uint8_t calculated_checksum = (0xFF + Data_H + Data_L) & 0xFF;
 
-            if (calculated_checksum == checksum) {
-              uint16_t distance = (Data_H << 8) | Data_L;
-              return distance;
-            } else {
-                printf("Checksum incorrecto en el paquete \n");
-                printf("\n");
-              }
-        } else {
-            printf("Paquete de datos incorrecto \n");
-            printf("\n");
-          }
-    }
-    return 0; // Devolver 0 si no se pudo procesar ningún paquete correctamente
+      if (calculated_checksum == checksum) {
+        uint16_t distance = (Data_H << 8) | Data_L;
+        return distance;
+      } else {
+          printf("Checksum incorrecto en el paquete \n");
+          printf("\n");
+        }
+    } else {
+        printf("Paquete de datos incorrecto \n");
+        printf("\n");
+      }
+  }
+  return 0; // Devolver 0 si no se pudo procesar ningún paquete correctamente
 }
 
 // Función que lee y procesa los datos del sensor
 uint16_t read_ultrasonic_sensor(void) {
-    // Buffer para almacenar los datos del UART
-    uint8_t data[BUF_SIZE];
+  // Buffer para almacenar los datos del UART
+  uint8_t data[BUF_SIZE];
 
-    // Leer datos del sensor y limpiar buffer
-    int length = uart_read_bytes(UART_NUM, data, 92, 100 / portTICK_PERIOD_MS);
-    uart_flush_input(UART_NUM);
+  // Leer datos del sensor y limpiar buffer
+  int length = uart_read_bytes(UART_NUM, data, 92, 100 / portTICK_PERIOD_MS);
+  uart_flush_input(UART_NUM);
 
-    if (length > 0) {
-        // Procesar los datos según el protocolo del sensor y devolver la distancia
-        return process_data_packet(data, length);
-    } else {
-        printf("No data received\n");
-        printf("\n");
-      }
+  if (length > 0) {
+    // Procesar los datos según el protocolo del sensor y devolver la distancia
+    return process_data_packet(data, length);
+  } else {
+      printf("No data received\n");
+      printf("\n");
+    }
 
-    // Si no se reciben datos, devolver 0
-    return 0;
+  // Si no se reciben datos, devolver 0
+  return 0;
 }
 
 //Función que actualiza la media móvil con una nueva lectura
-  bool media_sense_read(uint8_t canal) {   //Configura los pines de control A, B y C del MUX
-    switch (canal) {
-        case 0: //Sensor 1 en el pin Y0
-          gpio_set_level(PIN_A, 0);
-          gpio_set_level(PIN_B, 0);
-          gpio_set_level(PIN_C, 0);
-          break;
-        case 1: //Sensor 2 en el pin Y1
-          gpio_set_level(PIN_A, 1);
-          gpio_set_level(PIN_B, 0);
-          gpio_set_level(PIN_C, 0);
-          break;
-        case 2: //Sensor 3 en el pin Y2
-          gpio_set_level(PIN_A, 0);
-          gpio_set_level(PIN_B, 1);
-          gpio_set_level(PIN_C, 0);
-          break;
-        case 3: //Sensor 4 en el pin Y3
-          gpio_set_level(PIN_A, 1);
-          gpio_set_level(PIN_B, 1);
-          gpio_set_level(PIN_C, 0);
-          break;
-        case 4: //Sensor 5 en el pin Y4
-          gpio_set_level(PIN_A, 0);
-          gpio_set_level(PIN_B, 0);
-          gpio_set_level(PIN_C, 1);
-          break;
-    }
-    printf("Canal actual %d \n", canal);
+bool media_sense_read(uint8_t canal) {   //Configura los pines de control A, B y C del MUX
+  switch (canal) {
+    case 0: //Sensor 1 en el pin Y0
+      gpio_set_level(PIN_A, 0);
+      gpio_set_level(PIN_B, 0);
+      gpio_set_level(PIN_C, 0);
+      break;
+    case 1: //Sensor 2 en el pin Y1
+      gpio_set_level(PIN_A, 1);
+      gpio_set_level(PIN_B, 0);
+      gpio_set_level(PIN_C, 0);
+      break;
+    case 2: //Sensor 3 en el pin Y2
+      gpio_set_level(PIN_A, 0);
+      gpio_set_level(PIN_B, 1);
+      gpio_set_level(PIN_C, 0);
+      break;
+    case 3: //Sensor 4 en el pin Y3
+      gpio_set_level(PIN_A, 1);
+      gpio_set_level(PIN_B, 1);
+      gpio_set_level(PIN_C, 0);
+      break;
+    case 4: //Sensor 5 en el pin Y4
+      gpio_set_level(PIN_A, 0);
+      gpio_set_level(PIN_B, 0);
+      gpio_set_level(PIN_C, 1);
+      break;
+  }
+  printf("Canal actual %d \n", canal);
 
-    gpio_set_level(UART_INH_PIN, 0); //Habilitar el mux
-    vTaskDelay(pdMS_TO_TICKS(50));
+  gpio_set_level(UART_INH_PIN, 0); //Habilitar el mux
+  vTaskDelay(pdMS_TO_TICKS(50));
 
-    uint16_t val[N] = {0};
-    uint16_t suma = 0;
-    uint8_t cont = 0;
-    for (int i = 0; i < N; i++) {
-        val[i] = read_ultrasonic_sensor();
-        printf("Lectura %d ", i);
-        printf(": %d \n", val[i]);
-        if (val[i] == 0) {
-          cont = cont + 1;
-        }
-        suma += val[i];
-        vTaskDelay(pdMS_TO_TICKS(15));
+  uint16_t val[N] = {0};
+  uint16_t suma = 0;
+  uint8_t cont = 0;
+  for (int i = 0; i < N; i++) {
+    val[i] = read_ultrasonic_sensor();
+    printf("Lectura %d ", i);
+    printf(": %d \n", val[i]);
+    if (val[i] == 0) {
+      cont = cont + 1;
     }
-    if (cont == 4) {
-      cont = 0;
-    }
-    uint16_t media = suma/(N-cont); // Calcular la media de los resultados válidos
-    bool pig;
-    printf("Media: %d mm\n", media);
+    suma += val[i];
+    vTaskDelay(pdMS_TO_TICKS(15));
+  }
+  if (cont == 4) {
+    cont = 0;
+  }
+  uint16_t media = suma/(N-cont); // Calcular la media de los resultados válidos
+  bool pig;
+  printf("Media: %d mm\n", media);
 
-    if (media > umbral) {
+  if (media > umbral) {
+    pig = 1;
+  } else {
+      pig = 0;
+      if (media == 0) {
         pig = 1;
-    } else {
-        pig = 0;
-        if (media == 0) {
-          pig = 1;
-        }
       }
-    printf("Hay cerdo: %d \n", pig);
-    printf("\n");
+    }
+  printf("Hay cerdo: %d \n", pig);
+  printf("\n");
 
-    return pig; // Devolver la variable que decide si el cerdo está bebiendo
+  return pig; // Devolver la variable que decide si el cerdo está bebiendo
 }
 
 // Función principal
@@ -171,12 +171,12 @@ void app_main(void) {
   configure_uart();
 
   while (1) {
-      media_sense_read(canal);
+    media_sense_read(canal);
 
-      //Cambia al siguiente canal
-      canal = (canal + 1) % NUM_SENSE;
-      
-      // Esperar antes de la siguiente lectura
-      //vTaskDelay(pdMS_TO_TICKS(50));
-  }
+    //Cambia al siguiente canal
+    canal = (canal + 1) % NUM_SENSE;
+    
+    // Esperar antes de la siguiente lectura
+    //vTaskDelay(pdMS_TO_TICKS(50));
+}
 }
